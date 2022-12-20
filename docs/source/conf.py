08c471b03da0893e0c897cd52a922d74ea003e24
Lattawat <82594727+Lattawat@users.noreply.github.com>
2022-12-20 10:04:13 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,14 @@
+import datetime
+import string
+import sys
+import os
+
 # Configuration file for the Sphinx documentation builder.
 
-# -- Project information
-
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+# Package information
+project = "CACAO@HOME Robot"
+author = "Cacao Team"
+copyright = f"2022-{datetime.date.today().year}, {author}"
 
 release = '0.1'
 version = '0.1.0'
@@ -17,7 +21,16 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx_tabs.tabs'
 ]
+sphinx_tabs_valid_builders = ['linkcheck']
+sphinx_tabs_disable_tab_closing = True
+sphinx_tabs_disable_css_loading = True
+source_suffix = ".rst"
+pygments_style = "sphinx"
+
+master_doc = "index"
+language = 'en'
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
